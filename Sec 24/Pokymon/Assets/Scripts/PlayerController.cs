c4dc729806b7d6576c7037f3f02759f50d01b5b7
to86ju <to86ju@gmail.com>
2022-08-25 20:11:20 +0200

    Crear corrutine, animacion de Idel

--- Sec 24/Pokymon/Assets/Scripts/PlayerController.cs
+++ Sec 24/Pokymon/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private bool isMoving;
     public float speed;
     private Vector2 input;
+
+    private Animator __animator;
+
+    private void Awake()
+    {
+        __animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -15,14 +23,37 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
+
             if (input != Vector2.zero)
             {
+                __animator.SetFloat("Move x", input.x);
+                __animator.SetFloat("Move y", input.y);
+
                 var targetPosition = transform.position;
                 targetPosition.x += input.x;
                 targetPosition.y += input.y;
 
-                transform.position = targetPosition;
+                StartCoroutine(MoveTowards(targetPosition));
             }
         }
+    }
+
+    IEnumerator MoveTowards(Vector3 destination)
+    {
+        isMoving = true;
+
+        while (Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = destination;
+
+        isMoving = false;
     }
 }
